d452fb056f769d08ef7a89a4691b23e29d1b61b6
Chris Lewis <cmslewis@gmail.com>
2026-07-01 00:33:15 -0700

    add tina API URL

--- tina/config.ts
+++ tina/config.ts
@@ -9,10 +9,16 @@ const branch =
   process.env.HEAD ||
   process.env.VERCEL_GIT_COMMIT_REF ||
   "main";
+const clientId = process.env.NEXT_PUBLIC_TINA_CLIENT_ID;
+const contentApiUrlOverride =
+  clientId && branch
+    ? `https://content.tinajs.io/v1/content/${clientId}/github/${branch}`
+    : undefined;
 
 export default defineConfig({
   branch,
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  clientId,
+  contentApiUrlOverride,
   token: process.env.TINA_TOKEN,
   build: {
     outputFolder: "admin",
