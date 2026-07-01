d1933c36a6120fa8e573d6fc417c96be566f50b3
Chris Lewis <cmslewis@gmail.com>
2026-07-01 00:13:39 -0700

    npx tinacms dev

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/christopher.lewis/Documents/Staff & Ledger Website/tina/__generated__/.cache/1782886795328', url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
 export default client;
   
